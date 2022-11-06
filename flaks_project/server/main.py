155d27a7ba9bb3837b92d690be28011b0f0d8981
srazam <57676630+srazam@users.noreply.github.com>
2022-11-06 09:52:34 -0500

    Add files via upload

--- flaks_project/server/main.py
+++ flaks_project/server/main.py
@@ -1,1 +1,10 @@
+import requests
+import json
 
+api_url = 'http://127.0.0.1:8080/'
+
+response = requests.get(api_url)
+print(response.text)
+
+response = requests.post(api_url)
+print(response.text)
